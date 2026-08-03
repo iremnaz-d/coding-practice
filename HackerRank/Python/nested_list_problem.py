0a4148807b70abc2c65eeb675021edd0a2e2ded7
İrem Naz Durgut <iremnazdurgut4@gmail.com>
2026-08-03 16:02:44 +0300

    Find runner-up score in list solution

---
+++ HackerRank/Python/nested_list_problem.py
@@ -0,0 +1,31 @@
+#https://www.hackerrank.com/challenges/nested-list/problem?isFullScreen=true
+
+if __name__ == '__main__':
+
+    _list = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        _list.append([name, score])
+
+    _list.sort(reverse=False, key=lambda g: g[1])
+
+    s_list = []
+    first_grade = _list[0][1]
+    second_grade = 0
+
+    for i in _list:
+        if i[1] != first_grade:
+            second_grade = i[1]
+            break
+
+    for i in _list:
+        if i[1] == second_grade:
+            s_list.append(i)
+    s_list.sort(reverse=False, key=lambda x: x[0])
+
+    for i in s_list:
+        print(i[0])
+
+
+
