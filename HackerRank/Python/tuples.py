d02be1cc3c9ae935e6e3c4cc4eaba3cba70efccd
İrem Naz Durgut <iremnazdurgut4@gmail.com>
2026-08-04 14:56:22 +0300

    tuples challenge

--- HackerRank/Python/tuples.py
+++ HackerRank/Python/tuples.py
@@ -1,1 +1,8 @@
+#https://www.hackerrank.com/challenges/python-tuples/problem?isFullScreen=true
 
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+
+    _tuple = tuple(integer_list)
+    print(hash(_tuple))
