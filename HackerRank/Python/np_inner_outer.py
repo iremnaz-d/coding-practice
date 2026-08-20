35b7d82e4d36f6f4aed272781f4a252e3683a9d7
İrem Naz Durgut <iremnazdurgut4@gmail.com>
2026-08-20 15:01:06 +0300

    numpy inner and outer product problem

---
+++ HackerRank/Python/np_inner_outer.py
@@ -0,0 +1,10 @@
+#https://www.hackerrank.com/challenges/np-inner-and-outer/problem?isFullScreen=true
+
+import numpy as np
+
+if __name__ == '__main__':
+    A = np.array(list(map(int, input().split())))
+    B = np.array(list(map(int, input().split())))
+
+    print(np.inner(A, B), np.outer(A, B), sep="\n")
+
