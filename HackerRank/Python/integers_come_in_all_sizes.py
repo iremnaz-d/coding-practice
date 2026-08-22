45828bbf6c0ba2b22e81f84f75db6ef5e8ef83a9
İrem Naz Durgut <iremnazdurgut4@gmail.com>
2026-08-22 12:34:06 +0300

    integers come in all sizes problem

---
+++ HackerRank/Python/integers_come_in_all_sizes.py
@@ -0,0 +1,5 @@
+#https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a,b,c,d = int(input()),int(input()),int(input()),int(input())
+    print(a**b + c**d)
