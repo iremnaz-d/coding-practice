a1734174dc12615f44c627adcdef97aabc246548
İrem Naz Durgut <iremnazdurgut4@gmail.com>
2026-08-26 14:18:51 +0300

    validating named email address problem

---
+++ HackerRank/Python/validating_named_email.py
@@ -0,0 +1,17 @@
+#
+
+import email.utils
+import re
+
+if __name__ == '__main__':
+    pattern = r"^[a-zA-Z][a-zA-Z0-9._-]*@[a-zA-Z]+\.[a-zA-Z]{1,3}$"
+
+    for _ in range(int(input())):
+        mail = input()
+        parsed_email = email.utils.parseaddr(mail)
+
+        email_address = parsed_email[1]
+
+        if re.match(pattern, email_address):
+            print(email.utils.formataddr(parsed_email))
+
