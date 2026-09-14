e2c1b1065564440639677addccc8c123b7589b4f
İrem Naz Durgut <iremnazdurgut4@gmail.com>
2026-09-14 17:13:15 +0300

    display the first three rows of a dataframe

---
+++ LeetCode/_pandas/display_first_three_rows.py
@@ -0,0 +1,7 @@
+#https://leetcode.com/problems/display-the-first-three-rows/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+
+import pandas as pd
+
+
+def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    return employees.head(3)
