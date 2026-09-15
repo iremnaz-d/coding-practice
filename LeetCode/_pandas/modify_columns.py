d4f4cc685d7e04d6be96961dec4fe899c4b95996
İrem Naz Durgut <iremnazdurgut4@gmail.com>
2026-09-15 22:52:44 +0300

    modify columns

---
+++ LeetCode/_pandas/modify_columns.py
@@ -0,0 +1,7 @@
+#https://leetcode.com/problems/modify-columns/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+
+import pandas as pd
+
+def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['salary'] = employees['salary']*2
+    return employees
