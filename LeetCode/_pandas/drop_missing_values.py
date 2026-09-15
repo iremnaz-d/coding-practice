597845005c7cde57758f0ba4995e57d11c81c0ec
İrem Naz Durgut <iremnazdurgut4@gmail.com>
2026-09-15 22:50:10 +0300

    drop missing values

---
+++ LeetCode/_pandas/drop_missing_values.py
@@ -0,0 +1,7 @@
+#https://leetcode.com/problems/drop-missing-data/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+
+import pandas as pd
+
+
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    return students.dropna(axis=0, subset = 'name')
