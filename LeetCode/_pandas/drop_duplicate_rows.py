74c993e100e5de76494f234ae0a5af89b39b91db
İrem Naz Durgut <iremnazdurgut4@gmail.com>
2026-09-15 19:30:58 +0300

    drop duplicate rows

---
+++ LeetCode/_pandas/drop_duplicate_rows.py
@@ -0,0 +1,8 @@
+#https://leetcode.com/problems/drop-duplicate-rows/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+
+import pandas as pd
+
+
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    customers.drop_duplicates(subset='email', keep='first', inplace=True)
+    return customers
