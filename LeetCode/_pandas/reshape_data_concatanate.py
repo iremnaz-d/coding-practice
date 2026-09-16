1b9108daaef0ae00091e78da3788c58ad1b56229
İrem Naz Durgut <iremnazdurgut4@gmail.com>
2026-09-16 18:00:00 +0300

    reshape data concatenate

---
+++ LeetCode/_pandas/reshape_data_concatanate.py
@@ -0,0 +1,7 @@
+#https://leetcode.com/problems/reshape-data-concatenate/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+
+import pandas as pd
+
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    return pd.concat([df1, df2], axis=0)
